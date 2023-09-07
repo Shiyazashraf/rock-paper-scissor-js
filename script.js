e5b4591e286a76f5f06b7464dae7522cfa74bb4f
Shiyazashraf <shiyazashraft@gmail.com>
2023-09-07 18:07:17 +0530

    added and ui and modified the script

--- script.js
+++ script.js
@@ -1,9 +1,59 @@
 
+
+
+const buttons = document.querySelectorAll(".button");
+buttons.forEach(button => button.addEventListener('click',game))
+const playerscore = document.querySelector('#score-player');
+const compscore = document.querySelector('#score-comp');
+const results = document.querySelector(".results");
+const comp_option = document.querySelector("#comp-option")
+let score_player = 0;
+let score_comp = 0;
+
+ function game(e){
+
+    if(score_player != 5 && score_comp != 5){
+
+    
+    let player = e.target.value;
+    let computer = getComputerChoice();
+
+    
+
+    comp_option.textContent = computer;
+
+    let result = play(player,computer);
+    if (result == "You Win!"){
+        score_player++;
+        playerscore.textContent = score_player;
+        results.textContent = `${result}, ${player} beats ${computer}!! `;
+        
+    }
+    else if(result == "You Lose!"){
+        score_comp++;
+        compscore.textContent = score_comp;
+        results.textContent = `${result}, ${computer} beats ${player}!! `;
+        
+
+    }
+    else if(result == "it's a tie"){
+        results.textContent = `${result}, both chose same objects`;
+    }
+
+        
+    }
+    else if(score_player == 5){
+        results.textContent = "player won!";
+        return;
+    }else{
+        results.textContent = "computer won!";
+        return;
+    } 
+}
 //fn to get the choice of the computer
 function getComputerChoice(){
     let rps = ['rock' , 'paper', 'scissor'];
     let comp = rps[Math.floor(Math.random()* rps.length)];
-    console.log(comp);
     return comp;
 }
 
@@ -46,35 +96,7 @@ function play(player,comp){
             }
     }
 }
-// game function to keep score
-  function game(){
-    
-    let score_player = 0;
-    let score_comp = 0;
-    for(let i = 0; i < 5; i++){
-        player = window.prompt("enter Rock, Paper, or Scissors").toLowerCase();
-        comp = getComputerChoice();
-        let result = play(player,comp);
-        if (result == "You Win!"){
-            score_player+=1;
-            console.log(result);
-            
-        }
-        else if(result == "You Lose!"){
-            score_comp+=1;
-            console.log(result + ", " + comp + " beats " + player);
 
-        }
-        else {
-            console.log(result);
-        }
-        console.log("player score: " + score_player);
-        console.log("computer score: " + score_comp);
-    }
-  }
-let player;
-let comp;
-game();
 
 
 
